2ff88eb436dd883f4aa523142cda4ff44bd5ef9e
sooorajjj <sooorajjj@gmail.com>
2016-07-02 17:32:14 +0530

    Created new activity : MastersActivity and Settings Activity       * Given access to Admin and manager       * Moar Buttons !       * We are Killin it : Need Much Better Classifications with all these activities

---
+++ java/online/klok/mobpos/MastersActivity.java
@@ -0,0 +1,13 @@
+package online.klok.mobpos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MastersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masters);
+    }
+}
