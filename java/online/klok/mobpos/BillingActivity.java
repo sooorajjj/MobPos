b238b3b2c96cf58bff3e7df6546dd21069ee7e3c
sooorajjj <sooorajjj@gmail.com>
2016-07-02 18:33:46 +0530

    Added PayCollectActivity in BillingActivity        * New layout Finally!

--- java/online/klok/mobpos/BillingActivity.java
+++ java/online/klok/mobpos/BillingActivity.java
@@ -1,7 +1,11 @@
 package online.klok.mobpos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class BillingActivity extends Activity {
 
@@ -9,5 +13,17 @@ public class BillingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billing);
+
+
+        Button bPayCollect = (Button) findViewById(R.id.bPayCollect);
+        bPayCollect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent billingIntent = new Intent(BillingActivity.this, PayCollectActivity.class);
+                BillingActivity.this.startActivity(billingIntent);
+            }
+        });
+
+
     }
 }
---
+++ java/online/klok/mobpos/PayCollectActivity.java
@@ -0,0 +1,14 @@
+package online.klok.mobpos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PayCollectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_collect);
+
+    }
+}
