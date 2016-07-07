ac45505fa34d4bbe009f5a18a87e15c3b32c2b36
ZeroCool00 <luffyking786@gmail.com>
2016-07-07 22:40:15 +0530

    Add: split ActionBar support (1/3)
    Conflicts:
    	.idea/misc.xml
    	app/src/main/res/menu/testmenu.xml
    	java/online/klok/mobpos/AdminAreaActivity.java

--- java/online/klok/mobpos/AdminAreaActivity.java
+++ java/online/klok/mobpos/AdminAreaActivity.java
@@ -5,10 +5,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class AdminAreaActivity extends AppCompatActivity {
+public class AdminAreaActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bBilling;
+    private Button bReports;
+    private Button bMasters;
+    private Button bSettings;
+    private Toolbar toolbarBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,42 +31,76 @@ public class AdminAreaActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.collection_report);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+        initilize();
+        listener();
 
-        Button bBilling = (Button) findViewById(R.id.bBilling);
-        bBilling.setOnClickListener(new View.OnClickListener() {
+        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent adminIntent = new Intent(AdminAreaActivity.this, BillingActivity.class);
-                AdminAreaActivity.this.startActivity(adminIntent);
-            }
-        });
-
-        Button bReports = (Button) findViewById(R.id.bReports);
-        bReports.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent adminIntent = new Intent(AdminAreaActivity.this, ReportsActivity.class);
-                AdminAreaActivity.this.startActivity(adminIntent);
-            }
-        });
-        Button bMasters = (Button) findViewById(R.id.bMasters);
-        bMasters.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent adminIntent = new Intent(AdminAreaActivity.this, MastersActivity.class);
-                AdminAreaActivity.this.startActivity(adminIntent);
-            }
-        });
-        Button bSettings = (Button) findViewById(R.id.bSettings);
-        bSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent adminIntent = new Intent(AdminAreaActivity.this, SettingsActivity.class);
-                AdminAreaActivity.this.startActivity(adminIntent);
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.phone:
+                        Toast.makeText(getBaseContext(), "You selected Phone", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.computer:
+                        Toast.makeText(getBaseContext(), "You selected Computer", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.gamepad:
+                        Toast.makeText(getBaseContext(), "You selected Gamepad", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.camera:
+                        Toast.makeText(getBaseContext(), "You selected camera", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.video:
+                        Toast.makeText(getBaseContext(), "You selected video", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.email:
+                        Toast.makeText(getBaseContext(), "You selected email", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
             }
         });
     }
 
+    private void initilize() {
+        toolbarBottom = (Toolbar) findViewById(R.id.toolbar_bottom);
+        assert toolbarBottom != null;
+        toolbarBottom.inflateMenu(R.menu.main);
+        bBilling = (Button) findViewById(R.id.bBilling);
+        bReports = (Button) findViewById(R.id.bReports);
+        bMasters = (Button) findViewById(R.id.bMasters);
+        bSettings = (Button) findViewById(R.id.bSettings);
+    }
+
+    private void listener() {
+        bBilling.setOnClickListener(this);
+        bReports.setOnClickListener(this);
+        bMasters.setOnClickListener(this);
+        bSettings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bBilling) {
+            Intent adminIntent = new Intent(AdminAreaActivity.this, BillingActivity.class);
+            startActivity(adminIntent);
+        }
+
+        if (v.getId() == R.id.bReports) {
+            Intent adminIntent = new Intent(AdminAreaActivity.this, ReportsActivity.class);
+            startActivity(adminIntent);
+        }
+
+        if (v.getId() == R.id.bMasters) {
+            Intent adminIntent = new Intent(AdminAreaActivity.this, MastersActivity.class);
+            startActivity(adminIntent);
+        }
+
+        if (v.getId() == R.id.bSettings) {
+            Intent adminIntent = new Intent(AdminAreaActivity.this, SettingsActivity.class);
+            startActivity(adminIntent);
+        }
+    }
         @Override
         public void onBackPressed() {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
